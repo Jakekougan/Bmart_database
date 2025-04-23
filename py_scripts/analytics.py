ad0993b85b26f15d6b09828d2e3adc8f3dc67b26
haydenwarfield <hwarfiel@iwu.edu>
2025-04-23 18:52:33 -0500

    saving changes

---
+++ py_scripts/analytics.py
@@ -0,0 +1,105 @@
+from backend import get_connection
+
+def mostSoldProduct():
+    """
+    Author: Hayden Warfield
+
+    Retrieves and prints the most sold product across all of Bmart
+    """
+
+    #Connects to afhj Database
+    cnx = get_connection()
+    crs = cnx.cursor()
+
+    #Queries the Database for most sold product across all Bmart stores
+    item = crs.execute("SELECT product, SUM(quantity) AS Total Sold FROM afhj.order_items GROUP BY product ORDER BY Total Sold DESC LIMIT 1").fetchone()
+
+    #Prints the most sold product to the console
+    print("Most Sold Product across all of Bmart:")
+    print(item[0] + "Sold:")
+    print(item[1] + "units")
+
+def mostSoldProductByStore(storeNum):
+    """
+    Author: Hayden Warfield
+
+    Retrieves and prints the most sold product for a specific Bmart store
+
+    Parameter:
+        storeNum (int): The store number for which to find the most sold product
+    """
+
+    #Connects to afhj Database
+    cnx = get_connection()
+    crs = cnx.cursor()
+
+    #Queries the Database for the MOst sold product in the store
+    item = crs.execute("SELECT inventory.product, SUM(order_items.quantity) AS Total Sold FROM afhj.order_items RIGHT JOIN inventory ON order_items.inventory_id = inventory.inventory_id WHERE inventory.store = %s  GROUP BY inventory.product ORDER BY `Total Sold` DESC LIMIT 1", (storeNum,)).fetchone()
+
+    #Prints the most sold product in the specified store to the console.
+    print(item[0] + "Sold:")
+    print(item[1] + "units")
+
+
+def leastSoldByStore(storeNum):
+    """
+    Author: Hayden Warfield
+
+    Retrieves and prints the least sold product for a specific Bmart store
+
+    Parameters:
+        storeNum (int): The store number for which to find the least sold product
+    """
+
+    #Connects to afhj Database
+    cnx = get_connection()
+    crs = cnx.cursor()
+
+    #Queries the Database for the least sold product by store
+    item = crs.execute("SELECT inventory.product, SUM(order_items.quantity) AS Total Sold FROM afhj.order_items RIGHT JOIN inventory ON order_items.inventory_id = inventory.inventory_id WHERE inventory.store = %s  GROUP BY inventory.product ORDER BY `Total Sold` ASC LIMIT 1", (storeNum,)).fetchone()
+    print("Least Sold Item in Store #" + str(storeNum))
+    print(item[0] + "Sold:")
+    print(item[1] + "units")
+
+def lifetimeSalesbyStore(storeNum):
+    """
+    Author: Hayden Warfield
+
+    Retrieves and prints the lifetime sales for a specific Bmart store
+
+    Parameters:
+        storeNum (int): The store number for which to calculate lifetime sales.
+    """
+    #Connects to afhj Database
+    cnx = get_connection()
+    crs = cnx.cursor()
+
+    #Queries the Database for the Lifetime sales of the specified store
+    LifeSales = crs.execute("SELECT SUM(price) AS lifeSales FROM afhj.purchases LEFT JOIN afhj.order_items ON purchases.purchase_id = order_items.purchases LEFT JOIN inventory ON order_items.inventory_id  = inventory.inventory_id WHERE store = %s", (storeNum,)).fetchone()
+
+    #Prints lifetime sales and store number to console
+    print("Store #" + str(storeNum))
+    print("Lifetime Sales: $" + str(LifeSales[0]))
+
+def storeInventorySize(storeNum):
+    """
+    Author: Hayden Warfield
+    
+    Retrieves and prints the total inventory capacity for a specific Bmart store
+
+    Parameter:
+        storeNum (int): The store number for which to determine inventory size.
+    """
+
+    #Connects to afhj Database
+    cnx = get_connection()
+    crs = cnx.cursor()
+
+    #Queries the Database for Max Iventory size of the specified store
+    inventorySize = crs.execute("SELECT SUM(max_amt) AS maxInventory FROM afhj.inventory WHERE store = %s", (storeNum,)).fetchone()
+
+    #Prints the Store and Iventory size to the console
+    print("Store #" + str(storeNum))
+    print("Inventory Size: " + str(inventorySize[0]))
+
+
---
+++ py_scripts/project_part3.py
@@ -0,0 +1,79 @@
+from db_connection import get_connection
+import mysql.connector
+from datetime import datetime
+
+def stock(store, shipment, shipment_items):
+    """
+    Updates inventory and shipment records for a specific store upon receiving a shipment
+
+    This function:
+      - Marks the shipment as arrived in the database and records the arrival time
+      - Compares the shipment items against the reorder request to check for accuracy
+      - Logs differnces between ordered and received products or quantities
+      - Updates the inventory levels of each item received in the shipment
+
+    Parameters:
+
+        store (str):
+            The store ID where the shipment is being received
+        
+        shipment (str):
+            The shipment number associated with the incoming shipment
+        
+        shipment_items [list]:
+            A list of lists representing the products in the shipment. Each inner list must contain:
+            - UPC (int): The product's UPC code
+            - quantity (int): The number of units received for the product
+            Example: [[123456789012, 6], [987654321098, 7], [192837465748, 8]]  
+    """
+
+    cnx = get_connection()
+    with cnx.cursor() as crs:
+
+        #Updates database to indicate that an order arrived and when arrived where
+        try:
+
+            current_timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            crs.execute("UPDATE afhj.shipment SET actual_arrival = %s WHERE shipment_no = %s and store = %s", (current_timestamp, shipment, store))
+            print('Shipment #' + shipment + " arrived at Store #" + store + "at " + current_timestamp)
+            
+            #Gets the details of the original reorder request to Match sure the sipment contains the desired product and amount
+            #Retrives the items ordered
+            ordered_items = crs.execute("SELECT product, Product_qty FROM afhj.reorder_requests WHERE Store = %s AND shipment_no = %s", (store, shipment)).fetchall()
+            
+            for item in shipment_items:
+
+                #Compares to see if Order is correct 
+                if (item[0], item[1]) in ordered_items:
+
+
+                    crs.execute("UPDATE afhj.shipment SET delivered = 1 WHERE shipment = %s AND store = %s" , (shipment, store))
+                    print("Contained:" + ordered_items[1] + " " + ordered_items[0])
+
+                #Prints out What Item is wrong in contents
+                else:
+                    wrongitemName = crs.execute("SELECT name FROM afhj.bmart_products WHERE upc = %s", (item[0])).fetchone()
+                    orderedItemName = crs.execute("SELECT name FROM afhj.bmart_products WHERE upc = %s", (ordered_items[0])).fetchone()
+                    print("Wrong Product/Quantity!")
+                    print("Order Contained: " + item[1] + " " + wrongitemName[0])
+                    print("Was supposed to contain: " + ordered_items[1] + " " + orderedItemName[0])
+                
+
+            for item in shipment_items:
+            #Gets quantities and Products regarding the new stock
+    
+                amountToAdd = item[1]
+                product = item[0]
+                crs.execute("UPDATE afhj.inventory SET curr_amount = curr_amount + %s WHERE store = %s AND product_num = %s", (amountToAdd ,store, product))
+
+                inventoryInfo = crs.execute("SELECT store, curr_amt, max_amt FROM iventory WHERE product_num = %s", (item[0])).fetchone()
+                productName = crs.execute("SELECT name FROM bmart_products WHERE upc = %s", (item[0])).fetchone()
+                
+                print("Store #" + inventoryInfo[0] + " now has " + inventoryInfo[1] + "/" + inventoryInfo[2] + " " + productName[0])
+
+        except mysql.connector.Error as err:
+
+            print("Error adding new Inventory to the Database", crs.statement, str(err))
+
+    crs.commit()
+    cnx.close()
